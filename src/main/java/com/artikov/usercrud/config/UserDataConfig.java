48e33f79f4f93d89253a5835112950ffe00be2b2
tahirartikov <at0c102619@techmahindra.com>
2022-04-27 18:57:19 +0500

    CRUD completed

---
+++ src/main/java/com/artikov/usercrud/config/UserDataConfig.java
@@ -0,0 +1,13 @@
+package com.artikov.usercrud.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UserDataConfig {
+    @Bean
+    public OpenAPI myOpenAPI(){
+        return new OpenAPI();
+    }
+}
